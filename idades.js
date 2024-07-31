56f9a4c155f768c30d1cbb409db7908ea50624fe
evelynriandrade6 <eveyandrade0.3@gmail.com>
2024-07-31 13:54:59 -0300

    update

---
+++ idades.js
@@ -0,0 +1,16 @@
+const num = 17
+if (num < 0){
+    console.log(`Erro!`)
+}
+else if(num <= 13){
+    console.log(`Idade de criança!`)
+}
+else if(num <= 18){
+    console.log(`Idade de adolescente!`)
+}
+else if(num <= 65){
+    console.log(`Idade de adulto!`)
+}
+else{
+    console.log(`Idade de idoso!`)
+}
